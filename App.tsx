f1055a3a2c9f3bedf6ccf7f1d0359143e3f25149
Avia Weiss <avia7777@gmail.com>
2021-07-29 21:34:43 +0300

    Add android permission for http requests (without ssl)

--- App.tsx
+++ App.tsx
@@ -2,30 +2,24 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { SafeAreaView, View, FlatList, Text, StatusBar } from 'react-native';
 import Post from './components/Post';
 import styles from './style';
+import feedService from './feed/FeedService';
 import { v4 as uuidv4 } from 'uuid';
 import { pagesStore } from './feed/FeedStore';
-import { observer } from 'mobx-react';
 
+const App = () => {
 
-const App = observer(() => {
-
+  const [pages, setPages] = useState([]);
   const [items, setItems] = useState([]);
 
-  const addItems = () => {
-    console.log(`There are ${items.length} posts on feed!!!`);
-    const nextPage = pagesStore.nextPage;
-    if (nextPage) {
-      setItems((i) =>{
-        // return i.slice(-50).concat(feed);
-        return i.concat(nextPage); // no slicing
-      });
-    }
-  }
-
-  const renderItem = ({ item }) => {
-    // console.log("Key ==> ", item.key);
-      return <Post {...item} />
-  }
+  // const changeFeed = (list) => {
+  //   let feed = list.map(item => {
+  //     const key = uuidv4();
+  //     return {...item, key: key};
+  //   })
+  //   setItems((i) =>{
+  //     return i.slice(-50).concat(feed);
+  //   });
+  // }
 
   useEffect(() => {
     (async ()=>{
@@ -34,19 +28,52 @@ const App = observer(() => {
     })();
   }, [])
 
+  // useEffect(() => {
+  //   let initFeedPages = feedService.getPages();
+  //   changeFeed(initFeedPages.shift());
+  //   setPages(initFeedPages);
+  // }, [])
+
+  const renderItem = useCallback(
+    ({ item }) => {
+      // console.log("Key ==> ", item.key);
+      return <Post {...item} />
+    },
+    []
+  );
+
+  const addItems = () => {
+    console.log(`[sensify] There are ${items.length} posts on feed!!!`);
+    const nextPage = pagesStore.nextPage;
+    console.log(`[sensify] next page: ==> ${JSON.stringify(nextPage)}`);
+    if (nextPage) {
+      console.log("[sensify] next pages launced");
+      let feed = nextPage.map(item => {
+            const key = uuidv4();
+            return {...item, key: key};
+          })
+      setItems((currentItems) =>{
+        console.log(`[sensify] set adding feed: ${JSON.stringify(feed)}`);
+        // return i.slice(-50).concat(feed);
+        return currentItems.concat(feed); // no slicing
+      });
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={{color: 'white'}}>Avia is the king!</Text>
+      </View>
       <FlatList
           data={items}
           renderItem={renderItem}
           onEndReached={addItems}
           removeClippedSubviews={true}
-          keyExtractor={() => uuidv4()}
         />
     </SafeAreaView>
   );
 }
-)
 
 export default App;
 
--- feed/FeedStore.tsx
+++ feed/FeedStore.tsx
@@ -27,17 +27,21 @@ export default class FeedStore {
 
     public async fetchPages() {
         try {
-            const resObj = await fetch('http://thymos.life/feed');
+            const resObj = await fetch('http://thymos.life/feed', 
+            {headers: {'Accept': 'application/json'}});
+            // const resObj = await fetch('http://thymos.life/feed');
+            // https://jsonplaceholder.typicode.com/posts
             const res = await resObj.json();
             runInAction(()=>{
                 this.pages = [];
+                console.log(`[sensify] res ==> ${JSON.stringify(res)}`);
                 res.forEach(page => {
                     this.pages.push(page.posts)
                 });
             });
-            console.log("Success to fetch feed pages");
+            console.log("[sensify] Success to fetch feed pages");
         } catch(e) {
-            console.log("Failed to fetch feed pages - ", e)
+            console.log("[sensify] Failed to fetch feed pages - ", e)
         }
     }
 
@@ -48,7 +52,7 @@ export default class FeedStore {
         } else {
             this.pagesIndex = 0;
             console.log("########################################");
-            console.log("Starting over from the top!");
+            console.log("[sensify] Starting over from the top!");
             console.log("########################################");
             return this.pages[this.pagesIndex];
         }
